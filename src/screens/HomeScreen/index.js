e5071bbaad2332c74aac8f958bbca3d7831a10d7
navanee1609 <navaneethanvs18@gmail.com>
2024-04-26 09:23:53 +0530

    Update home screen with new design and assets; remove favicon, logos, and manifest file from public directory
    * Replace existing home screen layout with new design
    * Add new elements including logo, title, subtitle, and button

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -4,8 +4,16 @@ export const HomeScreen=()=>{
     return (
         <div className='home-container'>
             <div className='left-container'>
-                <h2>Left Container</h2>
-
+                <div className='item-container'>
+                <img src='logo.png' alt='logo'/>
+                <h1>Code-Craft</h1>
+                <h2>Code.Compile.Debug</h2>
+                <button>
+                    <span className='material-icons'>add</span>
+                    <span>new playground</span>
+                </button>
+                </div>
+                
             </div>
             <div className='right-container'>
                 <h2>Right Container</h2>
