fc10c5ac18bb7d3aac6f88b11a9a5bfedd7c8310
navanee1609 <navaneethanvs18@gmail.com>
2024-04-30 12:27:02 +0530

    Add cards container to display multiple project cards in HomeScreen RightComponent
    This diff adds a new section to the HomeScreen's RightComponent that displays multiple project cards. The cards are contained within a new `cards-container` div

--- src/screens/HomeScreen/RightComponent/index.js
+++ src/screens/HomeScreen/RightComponent/index.js
@@ -27,6 +27,66 @@ function index() {
           </button>
         </div>
         </div>
+
+        {/* cards container */}
+        <div className='cards-container'>
+          <div className='card'>
+            
+            <img src='logo.png' alt='codeCraft'/>
+            <div className='title-container'>
+              <span>{'Heap Implementation'}</span>
+              <span>Language : {'Java'}</span>
+            </div>
+          
+            
+            <div style={{display:'flex', gap:'10px'}}>
+            <span className='material-icons'>delete</span>
+            <span className='material-icons'>edit</span>
+            </div>
+          </div>
+          <div className='card'>
+            
+            <img src='logo.png' alt='codeCraft'/>
+            <div className='title-container'>
+              <span>{'Heap Implementation'}</span>
+              <span>Language : {'Java'}</span>
+            </div>
+          
+            
+            <div style={{display:'flex', gap:'10px'}}>
+            <span className='material-icons'>delete</span>
+            <span className='material-icons'>edit</span>
+            </div>
+          </div>
+          <div className='card'>
+            
+            <img src='logo.png' alt='codeCraft'/>
+            <div className='title-container'>
+              <span>{'Heap Implementation'}</span>
+              <span>Language : {'Java'}</span>
+            </div>
+          
+            
+            <div style={{display:'flex', gap:'10px'}}>
+            <span className='material-icons'>delete</span>
+            <span className='material-icons'>edit</span>
+            </div>
+          </div>
+          <div className='card'>
+            
+            <img src='logo.png' alt='codeCraft'/>
+            <div className='title-container'>
+              <span>{'Heap Implementation'}</span>
+              <span>Language : {'Java'}</span>
+            </div>
+          
+            
+            <div style={{display:'flex', gap:'10px'}}>
+            <span className='material-icons'>delete</span>
+            <span className='material-icons'>edit</span>
+            </div>
+          </div>
+        </div>
         
       </div>
     </div>
