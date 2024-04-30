0d56780b2cf832ed4fccf0089d0bb463fe5a64d2
navanee1609 <navaneethanvs18@gmail.com>
2024-04-30 11:35:11 +0530

    "Add RightComponent with folder structure and header; update HomeScreen styles"
    This commit includes the following changes:
    
    1. The `RightComponent` has been updated to include a header with a title and an "Add Folder"

--- src/screens/HomeScreen/RightComponent/index.js
+++ src/screens/HomeScreen/RightComponent/index.js
@@ -2,7 +2,34 @@ import React from 'react'
 import './index.scss'
 function index() {
   return (
-    <div className='right-container'>Right Component</div>
+    <div className='right-container'>
+      <div className='header'>
+       <div className='title'>
+        <span>My</span> Playground
+       </div>
+        <button className='add-folder'>
+          <span className='material-icons'>add</span>
+          <span>New Folder</span>
+        </button>
+      </div>
+      <div className='folder-container'>
+        <div className='folder-header'>
+        <div className='folder-header-item'>
+          <span className='material-icons' style={{color:'#ffca29'}}>folder</span>
+          <span>{"DSA"}</span>
+        </div>
+        <div className='folder-header-item'>
+          <span className='material-icons'>delete</span>
+          <span className='material-icons'>edit</span>
+          <button>
+            <span className='material-icons'>add</span>
+            <span>New Playground</span>
+          </button>
+        </div>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
