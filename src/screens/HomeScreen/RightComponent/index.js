51e7da21c03337296f60a13dbe86cf12ffbe95a4
navanee1609 <navaneethanvs18@gmail.com>
2024-04-30 12:28:26 +0530

    "Add delete, edit icons to folder and card components in RightComponent of HomeScreen"
    This commit adds delete and edit icons to the folder and card components in the RightComponent of the HomeScreen. The icons are displayed using

--- src/screens/HomeScreen/RightComponent/index.js
+++ src/screens/HomeScreen/RightComponent/index.js
@@ -89,6 +89,83 @@ function index() {
         </div>
         
       </div>
+      <div className='folder-container'>
+        <div className='folder-header'>
+        <div className='folder-header-item'>
+          <span className='material-icons' style={{color:'#ffca29'}}>folder</span>
+          <span>{"DSA"}</span>
+        </div>
+        <div className='folder-header-item'>
+          <span className='material-icons'>delete</span>
+          <span className='material-icons'>edit</span>
+          <button>
+            <span className='material-icons'>add</span>
+            <span>New Playground</span>
+          </button>
+        </div>
+        </div>
+
+        {/* cards container */}
+        <div className='cards-container'>
+          <div className='card'>
+            
+            <img src='logo.png' alt='codeCraft'/>
+            <div className='title-container'>
+              <span>{'Heap Implementation'}</span>
+              <span>Language : {'Java'}</span>
+            </div>
+          
+            
+            <div style={{display:'flex', gap:'10px'}}>
+            <span className='material-icons'>delete</span>
+            <span className='material-icons'>edit</span>
+            </div>
+          </div>
+          <div className='card'>
+            
+            <img src='logo.png' alt='codeCraft'/>
+            <div className='title-container'>
+              <span>{'Heap Implementation'}</span>
+              <span>Language : {'Java'}</span>
+            </div>
+          
+            
+            <div style={{display:'flex', gap:'10px'}}>
+            <span className='material-icons'>delete</span>
+            <span className='material-icons'>edit</span>
+            </div>
+          </div>
+          <div className='card'>
+            
+            <img src='logo.png' alt='codeCraft'/>
+            <div className='title-container'>
+              <span>{'Heap Implementation'}</span>
+              <span>Language : {'Java'}</span>
+            </div>
+          
+            
+            <div style={{display:'flex', gap:'10px'}}>
+            <span className='material-icons'>delete</span>
+            <span className='material-icons'>edit</span>
+            </div>
+          </div>
+          <div className='card'>
+            
+            <img src='logo.png' alt='codeCraft'/>
+            <div className='title-container'>
+              <span>{'Heap Implementation'}</span>
+              <span>Language : {'Java'}</span>
+            </div>
+          
+            
+            <div style={{display:'flex', gap:'10px'}}>
+            <span className='material-icons'>delete</span>
+            <span className='material-icons'>edit</span>
+            </div>
+          </div>
+        </div>
+        
+      </div>
     </div>
   )
 }
